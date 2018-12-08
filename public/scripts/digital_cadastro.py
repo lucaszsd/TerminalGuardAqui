ba0096f33cc5cde26e1621badd92d8f0692787d0
LGCilento <lgc3@cin.ufpe.br>
2018-12-07 21:00:55 -0300

    modified:   public/scripts/digital_cadastro.py 	indentando o código

--- public/scripts/digital_cadastro.py
+++ public/scripts/digital_cadastro.py
@@ -17,37 +17,37 @@ from pyfingerprint.pyfingerprint import PyFingerprint
 	
 ##else							## Se conseguiu, flag de inicializacao = 0
 	## Espera leitura
-	while ( f.readImage() == False ):
-		pass
+while ( f.readImage() == False ):
+	pass
 
-	## Converter a imagem recebida para atributos e armazenar em charbuffer1
-	f.convertImage(0x01)
+## Converter a imagem recebida para atributos e armazenar em charbuffer1
+f.convertImage(0x01)
 
-	## Checar se digital ja cadastrada
+## Checar se digital ja cadastrada
+result = f.searchTemplate()
+positionNumber = result[0]
+
+## Digital ja cadastrada
+if ( positionNumber >= 0 ):
+	
+	## Enviar mensagem de digital ja cadastrada pro node
+	print(-1)
+	sys.stdout.flush()
+	print(-1)
+	sys.stdout.flush()
+
+## Realizar cadastro da digital
+else:
+
+	## Cria novo template
+	f.createTemplate()
+
+	## Salva template em novo lugar do array de templates
+	positionNumber = f.storeTemplate()
+	
+	## Envia mensagem de cadastro concluido pro node
 	result = f.searchTemplate()
-	positionNumber = result[0]
-
-	## Digital ja cadastrada
-	if ( positionNumber >= 0 ):
-		
-		## Enviar mensagem de digital ja cadastrada pro node
-		print(-1)
-		sys.stdout.flush()
-		print(-1)
-		sys.stdout.flush()
-
-	## Realizar cadastro da digital
-	else:
-
-		## Cria novo template
-		f.createTemplate()
-
-		## Salva template em novo lugar do array de templates
-		positionNumber = f.storeTemplate()
-		
-		## Envia mensagem de cadastro concluido pro node
-		result = f.searchTemplate()
-		print(result[0])
-		sys.stdout.flush()
-		print(result[1])
-		sys.stdout.flush()
+	print(result[0])
+	sys.stdout.flush()
+	print(result[1])
+	sys.stdout.flush()
